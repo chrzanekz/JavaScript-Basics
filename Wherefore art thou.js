9061262cdb5b4417d2114a1de80189aaa5f1ae09
chrzanekz <45237067+chrzanekz@users.noreply.github.com>
2022-10-13 11:21:15 +0200

    Create Wherefore art thou.js

---
+++ Wherefore art thou.js
@@ -0,0 +1,21 @@
+function whatIsInAName(collection, source) {
+  const arr = [];
+  // Only change code below this line
+    const souceKeys = Object.keys(source);
+  console.log (souceKeys);
+  // filter the collection
+  return collection.filter(obj => {
+    for (let i = 0; i < souceKeys.length; i++) {
+      if (!obj.hasOwnProperty(souceKeys[i]) ||
+          obj[souceKeys[i]] !== source[souceKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
